f119b1f0874f0ac9ff300c2822747389c2d8e438
rafal.nowocinski <vito24222@gmail.com>
2023-07-02 08:47:21 +0200

    Styles for iOS

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,7 +3,10 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 function GoalItems(props) {
     return (
       <View style={styles.goalItem}>
-        <Pressable android_ripple={{color: '#210644'}} onPress={props.onDeleteItem.bind(this, props.id)}>
+        <Pressable
+        android_ripple={{color: '#210644'}/* for android */}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({pressed}) => pressed && styles.pressedItem/* for iOS */}>
             <Text style={styles.goalText}>
               {props.text}
             </Text>
@@ -16,6 +19,9 @@ const styles = StyleSheet.create({
         margin: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc'
+    },
+    pressedItem: {
+      opacity: 0.5
     },
     goalText: {
         color: 'white',
